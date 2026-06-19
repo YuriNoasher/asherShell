4572b5b80e1c35a9107cad4370486158acb3dc86
YuriNoasher <yuri.noah.asher@gmail.com>
2026-06-19 00:05:35 -0600

    Crear NODOC.sh
    inicial (0.1.0)

---
+++ USO/NODOC.sh
@@ -0,0 +1,3 @@
+s__ALI="[ -f ~/.asherShell/.config/.ali/ali.sh ] && source -f ~/.asherShell/.config/.ali/ali.sh"
+s__P10K="source ~/.asherShell/.config/.p10k/temas-p10k.sh"
+s__NEOVIM="vim.opt.number = true\nvim.opt.relativenumber = true\nvim.opt.cursorline = true\nvim.opt.termguicolors = true\n\nvim.cmd.colorscheme("vim")\n\nvim.cmd.colorscheme("unokai")\nvim.cmd.colorscheme("slate")\n"
